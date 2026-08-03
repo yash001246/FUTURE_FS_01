cd78669a49de3db3fd8791a3fa10add94250704d
yash001246 <yashpareek766@gmail.com>
2026-08-03 14:23:33 +0530

    Fix API URL for production

--- portfolio/public/js/config.js
+++ portfolio/public/js/config.js
@@ -1,2 +1,2 @@
 // Change this when you deploy the backend somewhere other than localhost.
-const API_URL = "https://portfolio-8it0.onrender.com/";
+const API_URL = "https://portfolio-8it0.onrender.com";
